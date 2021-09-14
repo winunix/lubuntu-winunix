4b55a4aa25e46fd75bc038aad1d9d41489309f70
ailtonbsj <ailton.ifce@gmail.com>
2021-09-14 01:15:38 -0300

    fix no install recommends

--- modules/AppsEdu/superuser.sh
+++ modules/AppsEdu/superuser.sh
@@ -1,1 +1,2 @@
-apt install /usr/wx-center/modules/AppsEdu/*.deb -y 2>&1 >>/tmp/log$(date '+%Y%m%d.%H%M%S')
+apt install /usr/wx-center/modules/AppsEdu/*.deb -y --no-install-recommends 2>&1 >>/tmp/log$(date '+%Y%m%d.%H%M%S')
+
